fde1650aeecaabdbbc27455d4b9e703638ea5f39
s.b.markov <s.b.markov@tinkoff.ru>
2024-07-30 15:00:23 +0300

    mapstruct lib added

--- src/main/java/ru/smarkov/demo/domain/course/CourseController.java
+++ src/main/java/ru/smarkov/demo/domain/course/CourseController.java
@@ -1,10 +1,18 @@
 package ru.smarkov.demo.domain.course;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.smarkov.demo.domain.course.dto.CourseDto;
+import ru.smarkov.demo.domain.course.dto.ExtendedCourseDto;
+import ru.smarkov.demo.domain.course.mapper.SimpleSourceDestinationMapper;
+import ru.smarkov.demo.domain.person.Hero;
+import ru.smarkov.demo.domain.person.Scorpion;
+import ru.smarkov.demo.domain.person.Subzero;
+import ru.smarkov.demo.util.TestClass;
 
 import java.util.UUID;
 
@@ -13,8 +21,26 @@ import java.util.UUID;
 @RequestMapping("/course")
 public class CourseController {
 
+    @Autowired
+    private SimpleSourceDestinationMapper mapper;
+
+    @Autowired
+    private Subzero subzero;
+
     @GetMapping("/first")
-    public CourseDto home(){
-        return new CourseDto(UUID.randomUUID(), "course 1");
+    public CourseDto home() {
+        ExtendedCourseDto extCourse = new ExtendedCourseDto(UUID.randomUUID(), "course 1 the best");
+
+        Scorpion sc = new Scorpion();
+
+        if (sc.getClass().isAnnotationPresent(Hero.class)) {
+            System.out.println("Hero name: " + sc.getClass().getAnnotation(Hero.class).name());
+            System.out.println("best name:" + sc.getBestName());
+        }
+
+        System.out.println("Hero name: " + subzero.getName());
+        System.out.println("best live:" + subzero.getLive());
+
+        return mapper.destinationToSource(extCourse);
     }
 }
